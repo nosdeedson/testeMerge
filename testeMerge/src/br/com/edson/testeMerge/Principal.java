b8929c894b57e8e9f1fabbe45a504feafd156b78
nosdeedson <josemariazumira@gmail.com>
2020-11-22 18:18:52 -0300

    teste

--- testeMerge/src/br/com/edson/testeMerge/Principal.java
+++ testeMerge/src/br/com/edson/testeMerge/Principal.java
@@ -4,6 +4,10 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("eu sou a master");
+		
+		//TesteMerge t = new TesteMerge();
+		
+		System.out.println("agora sem o teste merge");
 
 	}
 
